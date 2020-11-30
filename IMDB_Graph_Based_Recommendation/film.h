533b980fe75ece1b6772fea1bfcff0469543f9cf
DavidLIUXLD <DaViD28106@outlook.com>
2020-11-30 01:03:18 -0500

    Add project files.

---
+++ IMDB_Graph_Based_Recommendation/film.h
@@ -0,0 +1,34 @@
+#include<string>
+using namespace std;
+class film
+{
+private:
+	int ID;
+	string title;
+	string genre;
+
+
+public:
+	film(int ID, string title, string genre)
+	{
+		this->ID = ID;
+		this->title = title;
+		this->genre = genre;
+	}
+
+	int getID()
+	{
+		return this->ID;
+	}
+
+	string getTitle()
+	{
+		return this->title;
+	}
+
+	string getGenre()
+	{
+		return this->genre;
+	}
+};
+
