7622f12807ff2f239cd79eaa69834c07134651da
DavidLIUXLD <47655919+DavidLIUXLD@users.noreply.github.com>
2020-12-07 20:53:55 -0500

    Update filmGraph.h

--- IMDB_Graph_Based_Recommendation/filmGraph.h
+++ IMDB_Graph_Based_Recommendation/filmGraph.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
+#include <queue>
 using namespace std;
 
 class filmGraph
@@ -13,16 +14,15 @@ class filmGraph
 
 private: 
 	//adjacentList for film nodes
-	unordered_map<film, vector<filmEdge>, filmHash> adjacentList;
-	//collection of all films 
+	unordered_map<int, vector<filmEdge>> adjacentList;
 	unordered_map<int, film> films;
-	
+
 	//return the inverse of jaccard similarity between two vectors,
 	//if the jaccard similiarity between given vectors is zero, return zero
-	float similarityScore(vector<int> listA, vector<int> listB)
+	double similarityScore(vector<int> listA, vector<int> listB)
 	{
 		unordered_set<int> uniqueItem;
-		int commonItem;
+		int commonItem = 0;
 		for (auto itr1 : listA)
 		{
 			uniqueItem.insert(itr1);
@@ -41,6 +41,18 @@ private:
 		return 0;
 	}
 
+	int findMin(unordered_map<int, double> distances, unordered_set<int> toVisit) {
+		double minDis = (double)INT_MAX;
+		int minItem = -1;
+		for (auto itr : toVisit) {
+			if (distances.at(itr) < minDis) {
+				minDis = distances.at(itr);
+				minItem = itr;
+			}
+		}
+		return minItem;
+	}
+
 public:
 	//given unordered_map filmCollections mapping film IDs to film titles, unordered_map genreCollection
 	//mapping from film IDs to a list of genre IDs, and unordered_map keywordCollection mapping from 
@@ -48,17 +60,17 @@ public:
 	filmGraph(unordered_map<int,string> filmCollection, unordered_map<int, vector<int>> genreCollection,
 			  unordered_map<int,vector<int>> keywordCollection) 
 	{
-		//temporary collection of films for better iteration access
+		//temperary collection of films for better iteration access
 		vector<film> storage;
 		for (int i = 0; i <= filmCollection.size() - 1; i++) 
 		{
-			film movie(i, filmCollection.at(i), genreCollection.at(i), keywordCollection.at(i));
-			films.insert(i) = movie;
+			film movie(filmCollection.at(i), genreCollection.at(i), keywordCollection.at(i));
+			films.insert(make_pair(i,movie));
 			storage.push_back(movie);
 		}
-		for (int i = 0; i <= films.size() - 1; i ++)
+		for (int i = 0; i <= storage.size() - 1; i ++)
 		{
-			for (int j = i + 1; j <= films.size() - 1; j ++)
+			for (int j = i + 1; j <= storage.size() - 1; j ++)
 			{	
 				//calculate similarityScore based on genres of the film
 				float similarityScore;
@@ -72,55 +84,60 @@ public:
 					vector<int> keywordTwo = storage.at(j).getKeywords();
 					similarityScore += 5 * this->similarityScore(keywordOne, keywordTwo);
 					filmEdge edge(i, j, similarityScore);
-					if (adjacentList.count(storage.at(i)))
+					if (adjacentList.count(i))
 					{
 						vector<filmEdge> similarFilms;
-						adjacentList.insert(make_pair(storage.at(i), similarFilms));
+						adjacentList.insert(make_pair(i, similarFilms));
 					}
-					if (adjacentList.count(storage.at(j)))
+					if (adjacentList.count((j)))
 					{
 						vector<filmEdge> similarFilms;
-						adjacentList.insert(make_pair(storage.at(j), similarFilms));
+						adjacentList.insert(make_pair(j, similarFilms));
 					}
-					adjacentList.at(storage.at(i)).push_back(edge);
-					adjacentList.at(storage.at(j)).push_back(edge);
+					adjacentList.at(i).push_back(edge);
+					adjacentList.at(j).push_back(edge);
 				}
 			}
 		}
 	}
-	
-		bool findConnection(vector<filmEdge> &edges, filmEdge &find)
+
+	vector<double> dijkstras(int sourceID)
 	{
-		for (int i = 0; i < edges.size(); i++)
-		{
-			if (edges.at(i) == find)
-				return true;
+		vector<double> distances(films.size(), (double)INT_MAX);
+		unordered_map<int, double> minDistances;
+		unordered_set<int> processed;
+		unordered_set<int> toProcess;
+		distances.at(sourceID) = 0;
+		for (int i = 0; i <= films.size() - 1; i++) {
+			if (i == sourceID) {
+				minDistances.insert(make_pair(i, 0));
+			}
+			else {
+				minDistances.insert(make_pair(i, (double)INT_MAX));
+			}
 		}
-		return false;
+		toProcess.insert(sourceID);
+		while (!toProcess.empty()) {
+			int currentFilm = findMin(minDistances, toProcess);
+			toProcess.erase(currentFilm);
+			processed.insert(currentFilm);
+			vector<filmEdge> relatedFilms = adjacentList.at(currentFilm);
+			for (auto itr : relatedFilms) {
+				int item = itr.getOtherFilm(currentFilm);
+				int newDis = minDistances.at(currentFilm) + itr.similarityScore;
+				int oldDis = minDistances.at(item);
+				if (!processed.count(item) and newDis < oldDis) {
+					minDistances.at(item) = newDis;
+					if (oldDis == (double)INT_MAX) {
+						toProcess.insert(item);
+					}
+				}
+			}
+		}
+		for (auto itr : minDistances) {
+			distances.at(itr.first) = itr.second;
+		}
+		return distances;
 	}
-
-	void dijkstras(film source)
-	{
-		int* d;
-		int* p;
-		unordered_set<film, filmHash> S;
-		unordered_set<film, filmHash> VS = films;
-
-		for (auto iter = VS.begin(); iter != VS.end(); iter++)
-		{
-			if (found)
-			d[i] = INT_MAX;
-			p[i] = -1;
-		}
-
-		
-		while (!VS.empty())
-		{
-			for(int i = 0; i < )
-
-		}
-
-	}
-	
 };
 
