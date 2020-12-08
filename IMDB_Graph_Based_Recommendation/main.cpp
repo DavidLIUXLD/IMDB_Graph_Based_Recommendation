577c11e39c0b2092c8b0b774a4237eb396d94b38
Darianviso21 <62153314+Darianviso21@users.noreply.github.com>
2020-12-07 20:35:05 -0500

    Update main.cpp

--- IMDB_Graph_Based_Recommendation/main.cpp
+++ IMDB_Graph_Based_Recommendation/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <iomanip>
-#include <chrono> 
+#include <chrono>
 #include "filmGraph.h"
+#include "filereader.h"
 using namespace std;
 
 
@@ -12,14 +13,15 @@ int main()
 	// start time
 	time_req = clock();
 
-
 	// STUFF TO TIME HERE
-
+	unordered_map<int, string> filmCollection;
+	unordered_map<int, vector<int>> genreCollection;
+	read_Meta(filmCollection, genreCollection);
 
 	// end time
 	time_req = clock() - time_req;
 	// how to calculate in seconds
-	cout << "Recommendations took " << (float)time_req / CLOCKS_PER_SEC << " seconds to calculate" << endl;
+	cout << "Reaading files took " << (float)time_req / CLOCKS_PER_SEC << " seconds to calculate" << endl;
 
 	return 0;
 }
