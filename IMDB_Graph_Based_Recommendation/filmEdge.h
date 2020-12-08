3a7a354cf3c01c53c177210dcabc8d9656387cc9
DavidLIUXLD <47655919+DavidLIUXLD@users.noreply.github.com>
2020-12-07 20:57:38 -0500

    Update filmEdge.h

--- IMDB_Graph_Based_Recommendation/filmEdge.h
+++ IMDB_Graph_Based_Recommendation/filmEdge.h
@@ -1,46 +1,30 @@
 #pragma once
-#include"film.h"
 class filmEdge
 {
 public:
 	int fromID;
 	int toID;
-	float similarityScore;
+	double similarityScore;
 
 	filmEdge()
 	{
 		this->similarityScore = -1;
 	}
 
-	filmEdge(int fromID, int toID, float similiarityScore)
+	filmEdge(int fromID, int toID, double similiarityScore)
 	{
 		this->fromID = fromID;
 		this->toID = toID;
 		similarityScore = similarityScore;
 	}
 
-	bool operator< (const filmEdge& other)
+	int getOtherFilm(int ID)
 	{
-		if (this->similarityScore < other.similarityScore) {
-			return true;
+		if (fromID == ID) {
+			return toID;
 		}
-		return false;
+		return fromID;
 	}
 
-	bool operator> (const filmEdge& other)
-	{
-		if (this->similarityScore > other.similarityScore) {
-			return true;
-		}
-		return false;
-	}
-
-	bool operator== (const filmEdge& other)
-	{
-		if (this->fromID == other.fromID and this->toID == other.toID) {
-			return true;
-		}
-		return false;
-	}
 };
 
