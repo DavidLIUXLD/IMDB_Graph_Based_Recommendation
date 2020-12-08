8d43a2340f3fc34e4cc6617aaf4f0c117cc7797d
Darianviso21 <62153314+Darianviso21@users.noreply.github.com>
2020-12-07 20:32:39 -0500

    Update filereader.h

--- IMDB_Graph_Based_Recommendation/filereader.h
+++ IMDB_Graph_Based_Recommendation/filereader.h
@@ -1,10 +1,11 @@
+
 //
 // Created by Dillan Maraj on 12/6/20.
 //
 
 #ifndef IMDB_GRAPH_BASED_RECOMMENDATION_FILEREADER_H
 #define IMDB_GRAPH_BASED_RECOMMENDATION_FILEREADER_H
-void read_Meta();
-void read_Keywords();
+void read_Meta(unordered_map<int, string> &filmCollection, unordered_map<int, vector<int>> &genreCollection);
+void read_Keywords(unordered_map<int, vector<int>> keywordCollection);
 
 #endif //IMDB_GRAPH_BASED_RECOMMENDATION_FILEREADER_H
