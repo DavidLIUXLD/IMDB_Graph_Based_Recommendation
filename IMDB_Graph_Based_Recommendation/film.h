a238e934e19763fc498f08e2bd0ba35f1bc441b5
DavidLIUXLD <47655919+DavidLIUXLD@users.noreply.github.com>
2020-12-07 20:54:49 -0500

    Update film.h

--- IMDB_Graph_Based_Recommendation/film.h
+++ IMDB_Graph_Based_Recommendation/film.h
@@ -11,33 +11,20 @@ private:
 	vector<int> genres;
 	//collection of keyword ID related to the film
 	vector<int> keywords;
-	//distance to the starting point reserved exclusively for Dijkstra shortest path algorithm
-	float distance;
 public:
 	//public ID for Hashing
-	int ID;
 	//default constructor
 	film() 
 	{
-		ID = -1;
 		title = "";
-		distance = -1.0;
 	};
 
 	//constructor with parameter
-	film(int ID, string title, vector<int> genres, vector<int> keywords)
+	film(string title, vector<int> genres, vector<int> keywords)
 	{	
-		this->ID = ID;
 		this->title = title;
 		this->genres = genres;
 		this->keywords = keywords;
-		distance = INT_MAX;
-	}
-	
-	//return the unique ID for the film
-	int getID()
-	{
-		return this->ID;
 	}
 
 	//return the title for the film
@@ -57,48 +44,7 @@ public:
 	{
 		return this->keywords;
 	}
-
-	//return the distance of the film from the starting point in dijkstra
-	float getDist() 
-	{
-		return this->distance;
-	}
-
-	//set the distance
-	void setDist(float distance)  
-	{
-		this->distance = distance;
-	}
-	//comparison operator overloading for std::priorityqueue compatability 
-	bool operator< (const film& other) const 
-	{
-		if (this->distance < other.distance) {
-			return true;
-		}
-		else if(this->ID < other.ID){
-			return true;
-		}
-		return false;
-	}
-
-	bool operator> (const film& other) const
-	{
-		if (this->distance > other.distance) {
-			return true;
-		}
-		else if (this->ID > other.ID) {
-			return true;
-		}
-		return false;
-	}
-
-	//equal operator overloading dor std::unordered_set and std::unordered_map compatability
-	bool operator== (const film& other) const
-	{
-		if (this->ID == other.ID) {
-			return true;
-		}
-		return false;
-	}
 };
+
+
 
